a88ffa46237aac7449b444c5127db06c91479de3
Akrit Shresths <akritshrestha@gmail.com>
2023-11-23 18:34:17 +0545

    first commit

---
+++ lua/akrit/core/options.lua
@@ -0,0 +1,39 @@
+local opt = vim.opt
+
+--line numbers
+opt.relativenumber = true
+opt.number = true
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+--backspace
+opt.backspace = "indent,eol,start"
+
+--clipboard
+opt.clipboard:append("unnamedplus")
+
+--split windows
+opt.splitright = true
+opt.splitbelow = true
+
+--encoding
+vim.scriptencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+opt.iskeyword:append("-")
